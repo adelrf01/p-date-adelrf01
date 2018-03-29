f734456c128d70a83ac09c60d67b8c9535269bc8
Aitor <adelrf01@estudiantes.unileon.es>
2018-03-29 14:08:22 +0200

    terceramodificacion

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -6,10 +6,55 @@ public class Date {
 	private int year;
 	private String nombre;
 	// Constructor mal programado: Permite crear fechas no validas
-	public Date(int day, int month, int year){
-		this.day = day;
-		this.month = month;
-		this.year = year;
+	public Date(int day, int month, int year)throws DateException{
+		this.year=year;
+		if(month<1||month>12){
+		throw new DateException("mes "+month+" no valido "+"valores posibles entre 1 y 12");
+		}
+		else{
+		this.month=month;
+		}
+		if(day<1){
+		throw new DateException("dia "+day+ " no valido "+" solo se pueden dias mayores que 1");
+		}
+		else{
+			switch(this.month){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				if(day>31){
+				throw new DateException("dia "+day+" no valido en este mes");
+				}
+				else{
+				this.day=day;
+				}
+			break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				if(day>30){
+				throw new DateException("dia "+day+" no valido en este mes");
+				}
+				else{
+				this.day=day;
+				}
+			break;
+			case 2:
+				if(day>28){
+				throw new DateException("dia "+day+" no valido en el mes de febrero");
+				}
+				else{
+				this.day=day;
+				}
+			break;
+			
+			}
+		}
 	}
 	public Date(){
 	this.day=01;
@@ -158,14 +203,14 @@ public class Date {
 		}
 	return verificacion;
 	}
-	/*String monthsLeft(){
-	StringBuilder months=new StringBuilder;
+	String monthsLeft(){
+	StringBuilder months=new StringBuilder();
 		for(int i=this.month+1;i<=12;i++){
-		month.append(this.getMonthName()+"\n");
+		months.append(this.getMonthName());
 		}
-	return months.toString;
+	return months.toString();
 	}
-	*/	
+		
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
 	}
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -4,9 +4,18 @@ public class MainDate {
 
 	public static void main(String[] args) {
 		Date today, tomorrow;
+	try{	
 		today = new Date(20, 3, 2018);
 		tomorrow = new Date(21, 3, 2018);
+		System.out.println(today.toString());
+		System.out.println(tomorrow.toString());
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYearIf(tomorrow));
-	}
+		System.out.println(today.getMonthName());
+	}catch (DateException e){
+		System.out.println(e.getMessage());
+		}
+		
+	
 
+}
 }
